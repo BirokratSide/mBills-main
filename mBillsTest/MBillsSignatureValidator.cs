ba7765a88a4172f30f101701baf7b1d1262bc34e
mirceta <kristijan.mirceta@gmail.com>
2018-04-10 13:14:56 +0200

    Verification works - encoding changed to UTF8

--- mBillsTest/MBillsSignatureValidator.cs
+++ mBillsTest/MBillsSignatureValidator.cs
@@ -45,38 +45,18 @@ namespace mBillsTest
         #endregion
 
         #region // public //
-        public bool VerifyTest(SMBillsAuthResponse response) {
-
-            string verificationMessage = getVerificationMessage(response);
-            byte[] hash = getSha256Hash(verificationMessage);
-            byte[] signature = Convert.FromBase64String(response.auth.signature); // hmmm
-
-            EnvelopedCms cryptoEngine = new EnvelopedCms(
-                    new ContentInfo(hash),
-                    new AlgorithmIdentifier(new System.Security.Cryptography.Oid("1.2.840.113549.1.1.11")));
-            return true;
-        }
-
         public bool Verify(SMBillsAuthResponse response) {
-
             RSACryptoServiceProvider csp = retrieveCryptoServiceProvider();
             string verificationMessage = getVerificationMessage(response);
             byte[] hash = getSha256Hash(verificationMessage);
-            byte[] signature = StringToByteArray(response.auth.signature); // hmmm
+            byte[] signature = HexStringToByteArray(response.auth.signature);
             return csp.VerifyHash(hash, CryptoConfig.MapNameToOID("SHA256"), signature);
         }
         #endregion
 
-
         #region // auxiliary //
         private RSACryptoServiceProvider retrieveCryptoServiceProvider() {
             X509Certificate2 cert = new X509Certificate2(this.publicKeyFile);
-
-            /*RSACryptoServiceProvider csp = (RSACryptoServiceProvider)cert.PublicKey.Key;
-            var enhCsp = new RSACryptoServiceProvider().CspKeyContainerInfo;
-            string mKeyContainerName = csp.CspKeyContainerInfo.KeyContainerName;
-            CspParameters cspparams = new CspParameters(enhCsp.ProviderType, enhCsp.ProviderName, mKeyContainerName);
-            csp = new RSACryptoServiceProvider(cspparams);*/
             RSACryptoServiceProvider csp = (RSACryptoServiceProvider)cert.PublicKey.Key;
             return csp;
         }
@@ -88,12 +68,12 @@ namespace mBillsTest
 
         private byte[] getSha256Hash(string message) {
             SHA256Managed sha256 = new SHA256Managed();
-            byte[] data = Encoding.Unicode.GetBytes(message);
+            byte[] data = Encoding.UTF8.GetBytes(message);
             byte[] hash = sha256.ComputeHash(data);
             return hash;
         }
 
-        public static byte[] StringToByteArray(string hex) {
+        public static byte[] HexStringToByteArray(string hex) {
             return Enumerable.Range(0, hex.Length)
                              .Where(x => x % 2 == 0)
                              .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
